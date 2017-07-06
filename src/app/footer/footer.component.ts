b7cd62e259eff4e4fa5d85155fb2e7fc08e65858
johannabzg <johanna.bouzige@gmail.com>
2017-07-06 15:29:05 +0200

    second_commit

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,10 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+        template: `
+<p>{{title}}</p>`
 })
 export class FooterComponent implements OnInit {
-
+  title = 'je suis un super footer !'
   constructor() { }
 
   ngOnInit() {
